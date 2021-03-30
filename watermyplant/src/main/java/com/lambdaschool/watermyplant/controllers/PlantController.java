f165b0748ef9e106573b5ba8cd3bac71198c01c9
Shazeen Fabius <shazeenfabius@Shazeens-MBP.lan>
2021-03-30 10:13:03 -0700

    complete delete plant by id endpoint

--- watermyplant/src/main/java/com/lambdaschool/watermyplant/controllers/PlantController.java
+++ watermyplant/src/main/java/com/lambdaschool/watermyplant/controllers/PlantController.java
@@ -32,6 +32,6 @@ public class PlantController
     public ResponseEntity<?> deletePlantById(@PathVariable long plantId)
     {
         plantService.delete(plantId);
-        return new ResponseEntity<>()
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
