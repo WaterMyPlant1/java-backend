ea4fda7af8980ec0501c1ce5b706bcd75c6e96c1
Shazeen Fabius <shazeenfabius@Shazeens-MBP.lan>
2021-03-31 07:21:20 -0700

    set up end point for creating new plant object

--- watermyplant/src/main/java/com/lambdaschool/watermyplant/controllers/PlantController.java
+++ watermyplant/src/main/java/com/lambdaschool/watermyplant/controllers/PlantController.java
@@ -3,10 +3,15 @@ package com.lambdaschool.watermyplant.controllers;
 import com.lambdaschool.watermyplant.models.Plant;
 import com.lambdaschool.watermyplant.services.PlantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -17,6 +22,7 @@ public class PlantController
     @Autowired
     PlantService plantService;
 
+//    get list of plants by id
     @GetMapping(value = "/{userId}",
         produces = {"application/json"})
     public ResponseEntity<?> getAllPlantsByUserId(
@@ -28,6 +34,25 @@ public class PlantController
             HttpStatus.OK);
     }
 
+    @PostMapping(value = "/newplant", consumes = "application/json")
+    public ResponseEntity<?> createNewPlant(@Valid @RequestBody Plant newplant) throws URISyntaxException
+    {
+        newplant.setPlantId(0);
+        newplant = plantService.save(newplant);
+
+        // set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newUserURI = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{plantid}")
+            .buildAndExpand(newplant.getPlantId())
+            .toUri();
+        responseHeaders.setLocation(newUserURI);
+
+        return new ResponseEntity<>(newplant, responseHeaders,
+            HttpStatus.OK);
+    }
+
+//    edit plant by id
     @PatchMapping(value = "/plant/{plantId}", consumes = "application/json")
     public ResponseEntity<?> editPlantById(@PathVariable long plantId, @RequestBody Plant editPlant)
     {
@@ -35,6 +60,7 @@ public class PlantController
         return new ResponseEntity<>(editPlant,HttpStatus.OK);
     }
 
+//    delete plant by id
     @DeleteMapping("/plant/{plantId}")
     public ResponseEntity<?> deletePlantById(@PathVariable long plantId)
     {
